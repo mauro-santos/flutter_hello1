afc275ae763f3034450be1a5c354022fd994d0cf
Mauro Antonio dos Santos <santos.mauroantonio@gmail.com>
2020-06-21 12:06:09 -0300

    - StateFulWidget: ações na AppBar com botões de ícones de lista e grid para alterar a apresentação da Stack.

--- lib/pages/hello_listview.dart
+++ lib/pages/hello_listview.dart
@@ -8,14 +8,41 @@ class Dog {
   Dog(this.nome, this.foto);
 }
 
-class HelloListView extends StatelessWidget {
+class HelloListView extends StatefulWidget {
+  @override
+  _HelloListViewState createState() => _HelloListViewState();
+}
+
+class _HelloListViewState extends State<HelloListView> {
+  bool _gridView = true;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(
-          "GridView",
+          "ListView",
         ),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.list),
+            onPressed: () {
+              print("Lista");
+              setState(() {
+                _gridView = false;
+              });
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.grid_on),
+            onPressed: () {
+              print("Grid");
+              setState(() {
+                _gridView = true;
+              });
+            },
+          ),
+        ],
         centerTitle: true,
       ),
       body: _body(),
@@ -31,37 +58,50 @@ class HelloListView extends StatelessWidget {
       Dog("Pastor", "assets/images/dog5.png"),
     ];
 
-    return GridView.builder(
-      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
-      itemCount: dogs.length,
-      itemBuilder: (BuildContext context, int index) {
-        Dog dog = dogs[index];
+    if (_gridView)
+      return GridView.builder(
+        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+        itemCount: dogs.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _itemView(dogs, index);
+        },
+      );
+    else
+      return ListView.builder(
+        itemExtent: 350,
+        itemCount: dogs.length,
+        itemBuilder: (BuildContext context, int index) {
+          return _itemView(dogs, index);
+        },
+      );
+  }
 
-        return Stack(
-          fit: StackFit.expand,
-          children: <Widget>[
-            _img(dog.foto),
-            Align(
-              alignment: Alignment.topLeft,
-              child: Container(
-                margin: EdgeInsets.all(12),
-                padding: EdgeInsets.all(8),
-                decoration: BoxDecoration(
-                  color: Colors.black45,
-                  borderRadius: BorderRadius.circular(16),
-                ),
-                child: Text(
-                  dog.nome,
-                  style: TextStyle(
-                    fontSize: 26,
-                    color: Colors.white,
-                  ),
-                ),
+  _itemView(List<Dog> dogs, int index) {
+    Dog dog = dogs[index];
+
+    return Stack(
+      fit: StackFit.expand,
+      children: <Widget>[
+        _img(dog.foto),
+        Align(
+          alignment: Alignment.topLeft,
+          child: Container(
+            margin: EdgeInsets.all(12),
+            padding: EdgeInsets.all(8),
+            decoration: BoxDecoration(
+              color: Colors.black45,
+              borderRadius: BorderRadius.circular(16),
+            ),
+            child: Text(
+              dog.nome,
+              style: TextStyle(
+                fontSize: 26,
+                color: Colors.white,
               ),
-            )
-          ],
-        );
-      },
+            ),
+          ),
+        )
+      ],
     );
   }
 
